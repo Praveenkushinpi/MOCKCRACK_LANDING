5a6ddeaaa8e87378f1f332c6aad9915c3e84578c
Praveen kumar <168343716+Praveenkushinpi@users.noreply.github.com>
2025-08-30 11:05:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
+// @ts-check
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  /* config options here */
+}
  
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
- 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = nextConfig
